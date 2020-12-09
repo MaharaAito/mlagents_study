104e1afd5e38a4530318c349987cb65d11f49d07
MaharaAito <mahara.aito.den@gmail.com>
2020-12-09 18:34:44 +0900

    initial commit

---
+++ Assets/RollerAgent.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerAgent : Agent
+{
+    public Transform target;
+    Rigidbody rBody;
+
+    public override void Initialize()
+    {
+        this.rBody = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        // Roller Agentが床から落ちているとき
+        if (this.transform.position.y < 0)
+        {
+            // 位置と速度をリセット
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+        }
+
+        // Target の位置のリセット
+        target.position = new Vector3(
+            Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    // 観察取得時に呼ばれる
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(target.position);
+        sensor.AddObservation(this.transform.position);
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    // 行動実行時に呼ばれる
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        // RollerAgentに力を加える
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rBody.AddForce(controlSignal * 10);
+
+        // RollerAgentがTargetの位置に到達したとき
+        float distanceToTarget = Vector3.Distance(
+            this.transform.position, target.position);
+        if (distanceToTarget < 1.42f)
+        {
+            AddReward(1.0f);
+            EndEpisode();
+        }
+
+        // RollerAgentが床から落ちた時
+        if (this.transform.position.y < 0)
+        {
+            EndEpisode();
+        }
+
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
